9a45df2097fe64e00399d72f8545f400711054d1
thienphuc5 <phucnguyenkid27@gmail.com>
2026-07-05 21:41:25 +0700

    Fix hydration mismatch

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body className="font-sans antialiased">
+      <body className="font-sans antialiased" suppressHydrationWarning>
         <AuthProvider>
           {children}
         </AuthProvider>
